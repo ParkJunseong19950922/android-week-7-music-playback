0eeb7ef46e6751f39e4c8c7034929af6a5741d16
ParkJunseong19950922 <andrewp0222@gmail.com>
2019-05-18 13:32:54 -0700

    update

--- app/src/main/java/com/ucsdextandroid1/musicsearch2/search/SearchActivity.java
+++ app/src/main/java/com/ucsdextandroid1/musicsearch2/search/SearchActivity.java
@@ -32,6 +32,10 @@ public class SearchActivity extends AppCompatActivity {
     private SearchAdapter searchAdapter;
     private String latestSearchTerm;
     private MusicControlsManager musicControls;
+    private MusicPlayer musicPlayer;
+
+    private int currentState;
+    private SongItem currentSong;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +67,7 @@ public class SearchActivity extends AppCompatActivity {
         arrowView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(MusicDetailsActivity.createIntent(SearchActivity.this));
+                startActivity(MusicDetailsActivity.createIntent(SearchActivity.this, currentState, currentSong));
             }
         });
 
@@ -71,6 +75,14 @@ public class SearchActivity extends AppCompatActivity {
     }
 
     private void setUpMusicControls() {
+//        musicPlayer = new MusicPlayer();
+//        musicPlayer.setStateChangedListener(new MusicPlayer.OnStateChangedListener() {
+//            @Override
+//            public void onStateChanged(int state) {
+//                musicControls.updateViewState(state);
+//            }
+//        });
+
         musicControls = MusicControlsManager.newUIBuilder()
                 .setVisibilityRootView(findViewById(R.id.am_controls_group))
                 .setLabelView(findViewById(R.id.am_controls_label))
@@ -84,23 +96,29 @@ public class SearchActivity extends AppCompatActivity {
         musicControls.setControlsClickListener(new MusicControlsManager.OnControlClickListener() {
             @Override
             public void onResumeClicked() {
-
+                //musicPlayer.resume();
+                MusicPlayerService.resume(SearchActivity.this);
             }
 
             @Override
             public void onPauseClicked() {
-
+                //musicPlayer.pause();
+                MusicPlayerService.pause(SearchActivity.this);
             }
         });
 
         searchAdapter.setOnItemClickListener(new OnItemClickListener<SongItem>() {
             @Override
             public void onItemClicked(SongItem item) {
+                //musicPlayer.play(item);
+                //musicControls.updateViewMetadata(item);
+                MusicPlayerService.play(SearchActivity.this, item);
 
+                musicControls.updateViewState(MusicPlayer.STATE_PLAYING);
             }
         });
 
-//        registerBroadcastReceiver();
+        registerBroadcastReceiver();
     }
 
     /**
@@ -131,38 +149,54 @@ public class SearchActivity extends AppCompatActivity {
         }
     }
 
-//    private void registerBroadcastReceiver() {
-//        LocalBroadcastManager broadcastManager = LocalBroadcastManager.getInstance(this);
-//        broadcastManager.registerReceiver(controlsBroadcastReceiver,
-//                new IntentFilter(MusicPlayerService.ACTION_STATE_CHANGED));
-//    }
-//
-//    private void unregisterBroadcastReceiver() {
-//        LocalBroadcastManager.getInstance(this).unregisterReceiver(controlsBroadcastReceiver);
-//    }
-//
-//    private BroadcastReceiver controlsBroadcastReceiver = new BroadcastReceiver() {
-//
-//        @Override
-//        public void onReceive(Context context, Intent intent) {
-//            if(intent.getAction() != null && MusicPlayerService.ACTION_STATE_CHANGED.equals(intent.getAction())) {
-//                int state = intent.getIntExtra(MusicPlayerService.EXTRA_PLAYBACK_STATE, MusicPlayer.STATE_STOPPED);
-//                SongItem item = intent.getParcelableExtra(MusicPlayerService.EXTRA_SONG);
-//
-//                if(musicControls != null) {
-//                    musicControls.updateViewState(state);
-//                    musicControls.updateViewMetadata(item);
-//                }
-//            }
-//        }
-//
-//    };
+    private void registerBroadcastReceiver(){
+        IntentFilter intentFilter = new IntentFilter(MusicPlayerService.ACTION_STATE_CHANGED);
+        LocalBroadcastManager.getInstance(this).registerReceiver(controlsBroadcastReceiver, intentFilter);
+    }
 
+    private void unregisterBroadcastReceiver(){
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(controlsBroadcastReceiver);
+    }
+
+    private BroadcastReceiver controlsBroadcastReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getAction() != null && MusicPlayerService.ACTION_STATE_CHANGED.equals(intent.getAction())) {
+                int state = intent.getIntExtra(MusicPlayerService.EXTRA_PLAYBACK_STATE, MusicPlayer.STATE_STOPPED);
+                SongItem item = intent.getParcelableExtra(MusicPlayerService.EXTRA_SONG);
+
+                if(musicControls != null) {
+                    musicControls.updateViewState(state);
+                    musicControls.updateViewMetadata(item);
+                }
+
+                currentState = state;
+                currentSong = item;
+            }
+        }
+
+    };
+
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//
+//        registerBroadcastReceiver();
+//    }
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//
+//        unregisterBroadcastReceiver();
+//    }
+//
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-//        unregisterBroadcastReceiver();
+        unregisterBroadcastReceiver();
     }
 
 }
